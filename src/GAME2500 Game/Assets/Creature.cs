98783d536bec3c8a5463207c1b1c681939e91990
Jonathan Doliver <doliverjonathan@gmail.com>
2020-11-16 01:22:45 -0500

    added keyboard movement and collisions

---
+++ src/GAME2500 Game/Assets/Creature.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : MonoBehaviour {
+    public int health;
+    public float maxAcceleration;
+
+    protected Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
--- src/GAME2500 Game/Assets/Villain.cs
+++ src/GAME2500 Game/Assets/Villain.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Villain : MonoBehaviour
+public class Villain : Creature 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate(){
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        Vector2 accelerationDirection = new Vector2(xInput, yInput).normalized;
+        rb2d.AddForce(accelerationDirection * maxAcceleration);
     }
 }
