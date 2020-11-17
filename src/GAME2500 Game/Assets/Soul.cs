7817ec9d84516bd2ea7b8f49e1364e08625837e9
Jonathan Doliver <doliverjonathan@gmail.com>
2020-11-17 14:50:03 -0500

    soul sprite moves between hosts

--- src/GAME2500 Game/Assets/Soul.cs
+++ src/GAME2500 Game/Assets/Soul.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Soul : MonoBehaviour {
     
+    public float travelSpeed; // how fast the soul sprite moves between hosts
+
     private Villain host; // the currently controllable enemy
-    private bool isTransferring; // whether the soul is visually moving between hosts
     private List<Villain> history; // log of previous hosts
-    private bool didChangeHost;
+    private bool didChangeHost; // whether the host has been set during the current update cycle
+    private bool isTransferring; // whether the soul is visually moving between hosts
     
     void Awake() {
         Villain.soul = this;
@@ -18,6 +20,13 @@ public class Soul : MonoBehaviour {
     void Start() {
         Villain firstHost = (Boss)FindObjectOfType(typeof(Boss));
         SetHost(firstHost);
+    }
+
+    void Update() {
+        if (host != null) {
+            float step = travelSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, host.transform.position, step);
+        }
     }
 
     void LateUpdate() {
