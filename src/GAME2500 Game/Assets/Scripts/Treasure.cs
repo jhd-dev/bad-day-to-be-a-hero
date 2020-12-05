0f977b2310a459ebc4ccaa09f70738f64fa71662
Jonathan <jmlehman97@earthlink.net>
2020-12-05 12:51:21 -0800

    Clicking on enemy makes you be them

--- src/GAME2500 Game/Assets/Scripts/Treasure.cs
+++ src/GAME2500 Game/Assets/Scripts/Treasure.cs
@@ -7,18 +7,35 @@ public class Treasure : MonoBehaviour
 {
     [SerializeField] int maxWealth = 100;
     [SerializeField] Slider slider;
+    [SerializeField] Sprite lowDamageSpr;
+    [SerializeField] Sprite medDamageSpr;
+    [SerializeField] Sprite hiDamageSpr;
+    SpriteRenderer spriteRenderer;
     int wealth = 100;
 
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         wealth = maxWealth;
     }
 
     public void GetPillaged(int amountStolen) {
         wealth -= amountStolen;
-        slider.value = (float)wealth / (float)maxWealth;
+        float relativeWealth = (float)wealth / (float)maxWealth;
+        ChooseSprite(relativeWealth);
+        slider.value = relativeWealth;
         if (wealth <= 0) {
             GameStatus.isGameOver = true;
         }
+    }
+
+    void ChooseSprite(float ratio)
+    {
+        if (ratio >= 2f / 3f)
+            spriteRenderer.sprite = lowDamageSpr;
+        else if (ratio >= 1f / 3f)
+            spriteRenderer.sprite = medDamageSpr;
+        else
+            spriteRenderer.sprite = hiDamageSpr;
     }
 }
