4f8d64f678a3a7c726694b61ee7c554f41911e25
Atom1cJay <74260546+Atom1cJay@users.noreply.github.com>
2020-12-05 17:15:29 -0500

    minion ai fix

--- src/GAME2500 Game/Assets/Scripts/Creatures/Boss.cs
+++ src/GAME2500 Game/Assets/Scripts/Creatures/Boss.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Boss : Villain {
     
+    private AIDestinationSetter destinationSetter;
+    private AIPath path;
+
+    private GameObject target;
+
     public int boneCount;
 
     void Awake() {
+
+        destinationSetter = GetComponent<AIDestinationSetter>();
+        path = GetComponent<AIPath>();
+        target = GameObject.FindGameObjectWithTag("HeroGoal");
+
+        destinationSetter.target = target.transform;
         //Villain.boss = this;
         if (Villain.soul != null) {
             Villain.soul.SetHost(this);
@@ -17,4 +29,19 @@ public class Boss : Villain {
         GameStatus.isGameOver = true;
         Destroy(gameObject);
     }
+
+    void FixedUpdate()
+    {
+        if (!gameObject.GetComponent<Boss>().isHost)
+        {
+            destinationSetter.enabled = true;
+            path.enabled = true;
+        }
+        else
+        {
+            destinationSetter.enabled = false;
+            path.enabled = false;
+        }
+    }
+
 }
