0362c7a4175bdd2514129ff39d8666354ee4d43b
Jonathan <jmlehman97@earthlink.net>
2020-12-06 12:18:40 -0800

    Title Screen / UI Scalability

---
+++ src/GAME2500 Game/Assets/Scripts/MenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] GameObject controlsSection;
+    [SerializeField] GameObject instructionsSection;
+    [SerializeField] GameObject loading;
+    [SerializeField] AudioSource click;
+
+    void Start()
+    {
+        click = GetComponent<AudioSource>();
+    }
+
+    public void StartGame()
+    {
+        click.Play();
+        loading.SetActive(true);
+        SceneManager.LoadScene(1);
+    }
+
+    public void ToggleControlsSection()
+    {
+        click.Play();
+        controlsSection.SetActive(!controlsSection.activeSelf);
+    }
+
+    public void ToggleInstructionsSection()
+    {
+        click.Play();
+        instructionsSection.SetActive(!instructionsSection.activeSelf);
+    }
+}
